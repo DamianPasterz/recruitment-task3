0c3cdb1536aeca3371e1b46b3cc0b90c0fde9ee2
Damian Pasterz <damian.pasterz@gmail.com>
2024-02-07 22:42:32 +0100

    fix

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,15 +1,17 @@
+import { terminalInstance } from '../index.js';
+
 export async function fetchQuote() {
   const response = await fetch('https://dummyjson.com/quotes/random');
   const data = await response.json();
-  appendToTerminal(data.quote);
+  terminalInstance.appendToTerminal(data.quote);
 }
 
 export function doubleNumber(value) {
   const num = parseFloat(value);
   if (!isNaN(num)) {
-    appendToTerminal(`${num} *  2 = ${num *  2}`);
+    terminalInstance.appendToTerminal(`${num} * 2 = ${num *  2}`);
   } else {
-    appendToTerminal('Invalid input. Please enter a number after "double" command.');
+    terminalInstance.appendToTerminal('Invalid input. Please enter a number after "double" command.');
   }
 }
 
